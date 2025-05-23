45e78e26479db6a86efd91a10c304b205560435b
Angel-MtzV <anm002@ucsd.edu>
2025-05-23 15:17:15 -0700

    fixed answers for slack message and decode questions

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,13 +6,13 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": # NEED TO DO!!
-        answer = "pcap !"
+        answer = "pcap"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No": # NEED TO DO!!
         answer = "Yes"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ": # NEED TO DO!!
